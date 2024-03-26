3baa29b4768ef6d4419e246e2fbedca22321bbce
Ilham  Maruf <ilham.maruf@widyatama.ac.id>
2024-03-26 14:39:36 +0700

    tambahan

--- src/components/HaloBambang.vue
+++ src/components/HaloBambang.vue
@@ -1,15 +1,22 @@
 <template>
     <h1>Hallo ini Bambang</h1><br>
     <h1>Umurmu segini bukan? {{ count }}</h1><br>
-    <button @click="increment">Count is: {{ count }}</button>
+    <v-text-field v-model="mhs.npm" label="NPM" variant="outlined"></v-text-field><br>
+    <v-text-field v-model="mhs.nama" label="Nama" variant="outlined"></v-text-field><br>
+    <v-text-field v-model="mhs.ipk" label="IPK" variant="outlined"></v-text-field><br>
+    <v-btn variant="outlined" @click="createMahasiswa" color="#9575CD" >
+  Submit
+</v-btn><br>
 </template>
-<script>
+<script lang="ts">
+import {Mahasiswa} from '@/model/Mahasiswa'
 export default {
   // Properties returned from data() become reactive state
   // and will be exposed on `this`.
   data() {
     return {
-      count: 0
+      count: 100,
+      mhs: new Mahasiswa (1234, 'Bambang', 3.4)
     }
   },
 
@@ -18,6 +25,12 @@ export default {
   methods: {
     increment() {
       this.count++
+    },
+    createMahasiswa() {
+        alert('Mahasiswa '+ this.mhs.nama +' berhasil dibuat')
+        this.mhs.npm = '';
+        this.mhs.nama = '';
+        this.mhs.ipk = '';
     }
   },
 
---
+++ src/model/Mahasiswa.ts
@@ -0,0 +1,11 @@
+export class Mahasiswa {
+    public npm:number
+    public nama:string
+    public ipk:number
+
+    constructor(npm: number, nama: string, ipk: number) {
+        this.npm = npm
+        this.nama = nama
+        this.ipk = ipk
+    }
+}
